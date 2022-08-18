a6d039c64f51a1ab4be008f639124c55602c7fe0
GeodesicDragon <christopherurquhart2022+github@gmail.com>
2022-08-18 20:16:44 +0100

    Version 1.9.10
    See release.

---
+++ MakeCigarettes/media/lua/server/items/CigaretteRecipeZombieSpawns.lua
@@ -0,0 +1,8 @@
+require 'Items/SuburbsDistributions'
+require 'Items/ProceduralDistributions'
+
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "GeoQOLCigaretteMagazine")
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.0001)
+
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "GeoQOLCigaretteMagazine")
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.0001)
